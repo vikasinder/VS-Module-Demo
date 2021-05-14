8df4b438ae8930a6f522c335475650dce839d586
Vikas sharma <narinder_sharma1982@yahoo.com>
2021-05-14 09:47:28 -0400

    updations

--- lab_file1.py
+++ lab_file1.py
@@ -1,22 +1,53 @@
+# Labfilemodule2 is imported ( all the files are imported )
 from labfile2module import *
+# Python package of colorama module is imported to add text styles
+from colorama import Fore, Back, Style
 
-
+# Instance of class is created
 member1 = area_parameters_volume()
 
-while input("Do You Want To Continue? [y/n] : ") == "y":
+print(Fore.LIGHTMAGENTA_EX)
+print(Back.RESET)
+print("\n********* Python Module and Packages Assignment **********\n")
+
+# This Loop is going to run unless user wants to exit the program
+
+while input(" Do You Want To Continue? [y/n] : ") == "y":
     try:
+        print(Fore.YELLOW)
+        print(Back.RESET)
         print("\n***** MENU *****\n")
+
+        # Input To get user choice:
+
         value = int(input(
-            "Choose what you want to calculate :\n Enter 1. To Calculate Area  \n Enter 2. To Calculate Perimeter  \n Enter 3. To Calculate Volume  \n Enter 0. For Exit : "))
+            "Choose what you want to calculate :\n\n Enter 1. To Calculate Area  \n Enter 2. To Calculate Perimeter  \n Enter 3. To Calculate Volume  \n Enter 0. For Exit : "))
+        # if - else condition
         if value == 1 or value == 2 or value == 3:
+
+            # Option function within class 'area_parameters_volume' is called
+            # value as an argument is passed to the function
+            # And returned  calculated value is assigned to answer Variable
+
             answer = member1.option(value)
-            print(f"Your Calculated Value is : {answer}")
+            print(Fore.LIGHTCYAN_EX)
+            print(Style.DIM)
+
+            # The result is then printed on to the screen
+
+            print(f"Your Calculated Value is : {answer}\n")
         elif value == 0:
+            print(Fore.RED)
             break
         else:
+            print(Fore.RED)
+            print(Back.WHITE)
             raise TypeError
     except TypeError:
-        print("You entered wrong Input. Please Try again.\n")
+        # If the value entered is not the accepted values it will raise this error
+        print("\n You entered wrong Input. Please Try again.\n")
     except ValueError:
+        print(Fore.RED)
+        # If the value entered is not the accepted values it will raise this error
         print(
-            f"Invalid data:  Please enter either 1 - 2 - 3 Or 0 to Exit.\n")
+            f"\nInvalid data:  Please enter either 1 - 2 - 3 Or 0 to Exit.\n")
--- labfile2module.py
+++ labfile2module.py
@@ -1,9 +1,11 @@
+# Class is declared That contains all the functions and variable
 class area_parameters_volume:
-
+    # Global single variable is declared that gets the calculated result
     calculate = 0
+# Option function is declared that is called on lab_file1 page
 
     def option(self, value):
-
+        # Condition to check value to be calculated bt the user:
         if(value == 1):
             return (self.area())
         elif(value == 2):
@@ -11,95 +13,250 @@ class area_parameters_volume:
         else:
             return (self.volume())
 
+# Area function is declared to calculate area and pass the calculated result on the screen
     def area(self):
-        print("\n **** Area **** \n")
+        print("\n ****Calculate Area **** \n")
 
         while True:
             try:
+                # Global keyword is used , so as to declare that calculate is a global variable
                 global calculate
                 area = int(input(
-                    "Choose what you want to calculate :\n Enter 1. Area of Square \n Enter 2. Area Of rectangle  \n Enter 3. Area of circle \n Enter 0. To Exit : "))
+                    "Choose what you want to calculate :\n\n Enter 1. Area of Square \n Enter 2. Area Of rectangle  \n Enter 3. Area of circle \n Enter 0. To Exit : "))
                 if(area == 1):
-                    side = float(input("Enter Side Of square :"))
-                    calculate = side ** 2
-                    return calculate
+                    # Loop will run unless the conditions are not met ( To keep check on redundancy)
+                    while True:
+                        try:
+                            side = float(input("\nEnter Side Of square : "))
+                            if side < 0:
+                                raise TypeError
+                            else:
+                                calculate = side ** 2
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please enter an integer or float value.\n")
+
                 elif(area == 2):
-                    length = float(input("Enter Length Of Rectangle : "))
-                    width = float(input("Enter width Of Rectangle : "))
-                    calculate = length*width
-                    return calculate
+                    # Loop will run unless the conditions are not met ( To keep check on redundancy)
+                    while True:
+                        try:
+                            length = float(
+                                input("\nEnter Length Of Rectangle : "))
+                            width = float(
+                                input("\nEnter width Of Rectangle : "))
+                            if length < 0:
+                                raise TypeError
+                            if width < 0:
+                                raise TypeError
+                            else:
+                                calculate = length*width
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry:{error}  Please enter an integer or float value.\n")
+
                 elif(area == 3):
-                    radius = float(input("Enter Radius Of circle : "))
-                    calculate = radius ** 2 * 3.14
-                    return calculate
+                   # Loop will run unless the conditions are not met ( To keep check on redundancy)
+                    while True:
+                        try:
+                            radius = float(
+                                input("\nEnter Radius Of circle : "))
+                            if radius < 0:
+                                raise TypeError
+                            else:
+                                calculate = radius ** 2 * 3.14
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry:{error}  Please enter an integer or float value.\n")
+
                 elif(area == 0):
                     return "Thanks For Using This Program"
                 else:
                     raise TypeError
             except TypeError:
-                print("You entered wrong Input. Please Try again.\n")
+                print("\nYou entered wrong Input. Please Try again.\n")
             except ValueError:
                 print(
-                    f"Invalid choice: {area}  Please enter either 1 - 2 - 3 \n")
+                    f"\nInvalid choice: {area}  Please enter either 1 - 2 - 3 \n")
+
+# Perimeter function is declared to calculate perimeter and pass the calculated result on the screen
 
     def perimeter(self):
-        print("\n **** Perimeter **** \n")
+        print("\n **** Calculate Perimeter **** \n")
+
+      # Loop will run unless the conditions are not met ( To keep check on redundancy)
 
         while True:
             try:
+              # Global keyword is used , so as to declare that calculate is a global variable
                 global calculate
                 Perimeter = int(input(
-                    "Choose what you want to calculate :\n Enter 1. Perimeter of Square \n Enter 2. Perimeter Of rectangle  \n Enter 3. Perimeter of circle \n Enter 0. To Exit : "))
+                    "Choose what you want to calculate :\n\n Enter 1. Perimeter of Square \n Enter 2. Perimeter Of rectangle  \n Enter 3. Perimeter of circle \n Enter 0. To Exit : "))
                 if(Perimeter == 1):
-                    side = float(input("Enter Side Of square :"))
-                    calculate = side * 4
-                    return calculate
+                    # Loop will run unless the conditions are not met ( To keep check on redundancy)
+
+                    while True:
+                        try:
+                            side = float(input("\nEnter Side Of square : "))
+                            if side < 0:
+                                raise TypeError
+                            else:
+                                calculate = side * 4
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please enter an integer or float value.\n")
+
                 elif(Perimeter == 2):
-                    length = float(input("Enter Length Of Rectangle : "))
-                    width = float(input("Enter width Of Rectangle : "))
-                    calculate = 2*(length+width)
-                    return calculate
+                  # Loop will run unless the conditions are not met ( To keep check on redundancy)
+
+                    while True:
+                        try:
+                            length = float(
+                                input("\nEnter Length Of Rectangle : "))
+                            width = float(
+                                input("\nEnter width Of Rectangle : "))
+                            if length < 0:
+                                raise TypeError
+                            if width < 0:
+                                raise TypeError
+                            else:
+                                calculate = 2*(length+width)
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry:{error}  Please enter an integer or float value.\n")
+
                 elif(Perimeter == 3):
-                    radius = float(input("Enter Radius Of circle : "))
-                    calculate = radius * 2 * 3.14
-                    return calculate
+                  # Loop will run unless the conditions are not met ( To keep check on redundancy)
+
+                    while True:
+                        try:
+                            radius = float(
+                                input("\nEnter Radius Of circle : "))
+                            if radius < 0:
+                                raise TypeError
+                            else:
+                                calculate = radius * 2 * 3.14
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please enter an integer or float value.\n")
+
                 elif(Perimeter == 0):
                     return "Thanks For Using This Program"
                 else:
                     raise TypeError
             except TypeError:
-                print("You entered wrong Input. Please Try again.\n")
+                print("\nYou entered wrong Input. Please Try again.\n")
             except ValueError:
                 print(
-                    f"Invalid choice: {Perimeter}  Please enter either 1 - 2 - 3 \n")
+                    f"\nInvalid choice: {Perimeter}  Please enter either 1 - 2 - 3 \n")
+# Volume function is declared to calculate volume and pass the calculated result on the screen
 
     def volume(self):
-        print("\n **** Volume **** \n")
+        print("\n **** Calculater Volume **** \n")
+        # Loop will run unless the conditions are not met ( To keep check on redundancy)
 
         while True:
             try:
+              # Global keyword is used , so as to declare that calculate is a global variable
                 global calculate
                 volume = int(input(
-                    "Choose what you want to calculate :\n Enter 1. Volume of Cube \n Enter 2. Volume Of Cylinder  \n Enter 3. Volume of Sphere \n Enter 0. To Exit : "))
+                    "Choose what you want to calculate :\n\n Enter 1. Volume of Cube \n Enter 2. Volume Of Cylinder  \n Enter 3. Volume of Sphere \n Enter 0. To Exit : "))
                 if(volume == 1):
-                    side = float(input("Enter Side Of cube : "))
-                    calculate = side ** 3
-                    return calculate
+
+                    # Loop will run unless the conditions are not met ( To keep check on redundancy)
+
+                    while True:
+                        try:
+                            side = float(input("\nEnter Side Of cube : "))
+                            if side < 0:
+                                raise TypeError
+                            else:
+                                calculate = side ** 3
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please enter an integer or float value.\n")
+
                 elif(volume == 2):
-                    radius = float(input("Enter Radius Of Cylinder : "))
-                    height = float(input("Enter height Of Cylinder : "))
-                    calculate = 3.14 * radius ** 2 * height
-                    return calculate
+
+                    # Loop will run unless the conditions are not met ( To keep check on redundancy)
+
+                    while True:
+                        try:
+                            radius = float(
+                                input("\nEnter Radius Of Cylinder : "))
+                            height = float(
+                                input("\nEnter height Of Cylinder : "))
+                            if radius < 0:
+                                raise TypeError
+                            if height < 0:
+                                raise TypeError
+                            else:
+                                calculate = 3.14 * radius ** 2 * height
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please enter an integer or float value.\n")
+
                 elif(volume == 3):
-                    radius = float(input("Enter Radius Of Sphere : "))
-                    calculate = (4/3) * 3.14 * radius**3
-                    return calculate
+
+                    # Loop will run unless the conditions are not met ( To keep check on redundancy)
+
+                    while True:
+                        try:
+                            radius = float(
+                                input("\nEnter Radius Of Sphere : "))
+                            if radius < 0:
+                                raise TypeError
+                            else:
+                                calculate = (4/3) * 3.14 * radius**3
+                                return calculate
+                        except TypeError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please provide a positive number.\n")
+                        except ValueError as error:
+                            print(
+                                f"\nInvalid Entry: {error}. Please enter an integer or float value.\n")
+
                 elif(volume == 0):
+                    # Returns "Thanks For Using This Program" if user wants to leave ther program
                     return "Thanks For Using This Program"
+
                 else:
+                    # If the value entered is not the accepted values it will raise this error
                     raise TypeError
             except TypeError:
-                print("You entered wrong Input. Please Try again.\n")
+                print("\nYou entered wrong Input. Please Try again.\n")
             except ValueError:
+                # If the value entered is not the accepted values it will raise this error
                 print(
-                    f"Invalid choice: {volume}  Please enter either 1 - 2 - 3 \n")
+                    f"\nInvalid choice: {volume}  Please enter either 1 - 2 - 3 \n")
